213ee1da95b0537c7c47a88a4c4cf6ecb1ff8be0
Malik M Alnakhaleh <jimalik2525@gmail.com>
2019-10-24 12:57:39 -0400

    Learning about components

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// Note: SizeUpTool for console resizing
+
+// console.log("App", App);
