411931f1264c8410fb17a24b3fd57016f33ba96d
Malik M Alnakhaleh <jimalik2525@gmail.com>
2019-11-03 22:19:51 -0500

    Finished with socialProfiles.js

--- src/data/socialProfiles.js
+++ src/data/socialProfiles.js
@@ -4,6 +4,20 @@ import linkedinIcon from "../images/lnikedin_icon.png";
 
 const SOCIAL_PROFILES = [
     {
-
+        id: 0,
+        link: "mailto:jimalik2525@gmail.com",
+        image: emailIcon
+    },
+    {
+        id: 1,
+        link: "https://github.com/malikmalna",
+        image: githubIcon
+    },
+    {
+        id: 2,
+        link: "https://www.linkedin.com/in/malik-mike-alnakhaleh-32b63818b",
+        image: linkedinIcon
     }
 ];
+
+export default SOCIAL_PROFILES;
