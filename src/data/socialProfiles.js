18c75823b6dfd33968a51aa06b74ad5df5652646
Malik M Alnakhaleh <jimalik2525@gmail.com>
2019-11-03 22:10:16 -0500

    Imported Icons into socialProfile.js

--- src/data/socialProfiles.js
+++ src/data/socialProfiles.js
@@ -1,3 +1,7 @@
+import emailIcon from "../images/email_icon.png";
+import githubIcon from "../images/github_icon.png";
+import linkedinIcon from "../images/lnikedin_icon.png";
+
 const SOCIAL_PROFILES = [
     {
 
