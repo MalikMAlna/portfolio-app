693e6e6a99137cad1e1f41a5d3cefb218b7aa3c1
Malik M Alnakhaleh <jimalik2525@gmail.com>
2019-11-05 20:41:11 -0500

    Added Notes

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // Note: SizeUpTool for console resizing
 
 // console.log("App", App);
+
+// components dev tools for better react development for browser
