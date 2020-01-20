d5cad8d12c8e4993bebbefc1a8e0eb99c0eded3f
Malik M Alnakhaleh <jimalik2525@gmail.com>
2020-01-20 13:44:51 -0500

    Added Index.css

--- src/SocialProfile.js
+++ src/SocialProfile.js
@@ -4,9 +4,9 @@ class SocialProfile extends Component {
     render() {
         const {link, image } = this.props.socialProfile;
         return(
-            <div>
-                <a href={link}><img src={image} alt="social-profile" /></a>
-            </div>
+            <span>
+                <a href={link}><img src={image} alt="social-profile" style={{width: 35, height: 35, margin: 10}} /></a>
+            </span>
 
         );
     }
