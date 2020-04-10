2ad9ed7e5b72fb949f63cbfeb5a541e8c26cd6e4
Mike A <jimalik2525@gmail.com>
2020-04-10 14:21:04 -0400

    Removed dist directory from remote repo

--- src/Project.js
+++ src/Project.js
@@ -4,9 +4,9 @@ class Project extends Component {
   render() {
     const { title, image, description, link } = this.props.project; // Can also be object and not project
     return (
-      <div style={{display: "inline-block", width: 300, margin: 10}}>
+      <div style={{display: "inline-block", width: 500, margin: 10}}>
         <h3>{title}</h3>
-        <img src={image} alt="application-pic" style={{width: 200, height: 120}}/>
+        <img src={image} alt="application-pic" style={{width: 200, height: 200}}/>
         <p>{description}</p>
         <a href={link}>{link}</a>
       </div>
